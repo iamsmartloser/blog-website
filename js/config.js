4e9f7e5c2853d270a2115e394a386628fbfd6522
iamsmartloser <121484818@qq.com>
2020-11-16 18:32:07 +0800

    初始化

--- js/config.js
+++ js/config.js
@@ -1,4 +1,6 @@
+
 const base_url='http://127.0.0.1:7001';
+const _userId = 'c203dc13-9f63-4be0-b0b7-54a410756089';
 
 const latest_articles_simple_url = base_url + '/article/findByPage';
 const latest_articles_detail_url = base_url + '/article/findByPage';
