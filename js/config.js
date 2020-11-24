036640e22a8094d2560e5c505c473c0d62c7fa43
iamsmartloser <121484818@qq.com>
2020-11-24 18:28:40 +0800

    初始化

--- js/config.js
+++ js/config.js
@@ -3,5 +3,5 @@ const base_url='http://127.0.0.1:7001';
 const _userId = '9628064f-d184-4975-a1a3-e7418b11f02a';
 
 const latest_articles_simple_url = base_url + '/article/findByPageWithAuth';
-const latest_articles_detail_url = base_url + '/article/findByPageWithAuth';
+const article_category_url = base_url + '/category/findAllWithAuth';
 const article_detail_url = base_url + '/article/findOneWithAuth';
