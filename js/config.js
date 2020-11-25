4d470b91c69051f70d08e68e979b6f687ed058d1
iamsmartloser <121484818@qq.com>
2020-11-25 17:52:46 +0800

    初始化

--- js/config.js
+++ js/config.js
@@ -2,6 +2,8 @@
 const base_url='http://127.0.0.1:7001';
 const _userId = '9628064f-d184-4975-a1a3-e7418b11f02a';
 
-const latest_articles_simple_url = base_url + '/article/findByPageWithAuth';
-const article_category_url = base_url + '/category/findAllWithAuth';
-const article_detail_url = base_url + '/article/findOneWithAuth';
+const latest_articles_simple_url = base_url + '/article/findByPageWithAuth';// 文章列表
+const article_category_url = base_url + '/category/findAllWithAuth';// 文章分类
+const article_detail_url = base_url + '/article/findOneWithAuth';// 文章详情
+const article_comment_list_url = base_url + '/comment/findByPage';// 文章评论列表
+const article_comment_create_url = base_url + '/comment/create';// 评论
