1ef335b2678e3df8b35ef98e9766519dea3b52ef
iamsmartloser <121484818@qq.com>
2020-12-16 17:28:38 +0800

    初始化

--- js/config.js
+++ js/config.js
@@ -1,5 +1,6 @@
-
-const base_url='http://47.108.152.55:7001';
+// const domain='http://www.smartloser.cn';
+const domain='';
+const base_url=domain+'/api';
 const _userId = '9628064f-d184-4975-a1a3-e7418b11f02a';
 
 const latest_articles_simple_url = base_url + '/article/findByPageWithAuth';// 文章列表
@@ -7,3 +8,5 @@ const article_category_url = base_url + '/category/findAllWithAuth';// 文章分
 const article_detail_url = base_url + '/article/findOneWithAuth';// 文章详情
 const article_comment_list_url = base_url + '/comment/findByPage';// 文章评论列表
 const article_comment_create_url = base_url + '/comment/create';// 评论
+
+
